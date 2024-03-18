8fda67638a5f70644ba47ceb2e6dd553019e867d
smalik21 <sahilmalik25007@gmail.com>
2024-03-18 23:51:38 +0530

    wrap app under context providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ParameterProvider } from './contexts/ParameterContext.tsx'
+import { ImageProvider } from './contexts/ImageContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <React.StrictMode>
+      <ParameterProvider>
+         <ImageProvider>
+            <App />
+         </ImageProvider>
+      </ParameterProvider>
+   </React.StrictMode>,
 )

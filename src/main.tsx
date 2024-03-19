575e5ee438d5edd58a6ab12099c84f004123decd
smalik21 <sahilmalik25007@gmail.com>
2024-03-19 23:58:26 +0530

    feat: :sparkles: add video provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { ParameterProvider } from './contexts/ParameterContext.tsx'
 import { ImageProvider } from './contexts/ImageContext.tsx'
+import { VideoProvider } from './contexts/VideoContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
       <ParameterProvider>
          <ImageProvider>
-            <App />
+            <VideoProvider>
+               <App />
+            </VideoProvider>
          </ImageProvider>
       </ParameterProvider>
    </React.StrictMode>,

5362f59f3a7e65ac57da9b19467d0c9d6ea0f620
smalik21 <sahilmalik25007@gmail.com>
2024-03-20 23:37:14 +0530

    feat: :sparkles: display loaded images/videos

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,17 +1,29 @@
-import sampleImg from '../assets/sample-image.svg'
-import sampleImage from '../assets/sample-img.svg'
+// import sampleImg from '../assets/sample-image.svg'
+// import sampleImage from '../assets/sample-img.svg'
+import { useParameter } from '../hooks/useParameter'
+import { useImage } from '../hooks/useImage'
+import { useVideo } from '../hooks/useVideo'
 
 const MainSection = () => {
-    return (
-        <main className='p-2 sm:p-4 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
-            <img className='' src={sampleImg} />
-            <img src={sampleImg} />
-            <img src={sampleImage} />
-            <img src={sampleImg} />
-            <img src={sampleImage} />
-            <img src={sampleImg} />
-        </main>
-    )
+
+   const { type } = useParameter()
+   const { images } = useImage()
+   const { videos } = useVideo()
+
+   return (
+      <main className='p-2 sm:p-4 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+         {type === "image" && images.map(image => {
+            return (
+               <img key={image.imageId} src={image.imageURL} />
+            )
+         })}
+         {type === "video" && videos.map(video => {
+            return (
+               <img key={video.videoId} src={video.normal.thumbnail} />
+            )
+         })}
+      </main>
+   )
 }
 
 export default MainSection

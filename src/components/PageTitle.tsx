77db3ae026d25cb251bc4b386da287f348d30860
smalik21 <sahilmalik25007@gmail.com>
2024-03-21 23:53:36 +0530

    feat: :sparkles: add a link to logo to navigate to home page

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,12 +1,14 @@
 import logo from "../assets/logo.svg"
-
+import { Link } from "react-router-dom"
 const PageTitle = () => {
-    return (
-        <span className="flex gap-2 items-center" id="page-title">
+   return (
+      <Link to={'/'}>
+         <span className="flex gap-2 items-center" id="page-title">
             <img className="size-6 sm:size-8 md:size-10" src={logo} alt="page-logo" />
             <span className="font-title text-lg sm:text-xl md:text-2xl">PicPulse</span>
-        </span>
-    )
+         </span>
+      </Link>
+   )
 }
 
 export default PageTitle

c9849c2232318276932a0cb8de68265a92c84bd6
smalik21 <sahilmalik25007@gmail.com>
2024-03-21 23:51:01 +0530

    fix: :bug: change relative path to absolute path
    to avoid background loading error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'heroSection': "url('./src/assets/wallpaper-blue.svg')"
-            },
-            fontFamily: {
-                'body': ['Inter', 'sans-serif'],
-                'title': ['Jockey One', 'sans-serif'],
-                'search': ['Julius Sans One', 'sans-serif'],
-            },
-            colors: {
-                'light': '#E3E3E3',
-                'gray-1': '#CFCFCF',
-                'gray-2': '#9A9A9A',
-                'dark': '#1C1C1C',
-            },
-            screens: {
-                'xs': '520px',
-            }
-        },
-    },
-    plugins: ["prettier-plugin-tailwindcss"],
+   content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            'heroSection': "url('/src/assets/wallpaper-blue.svg')"
+         },
+         fontFamily: {
+            'body': ['Inter', 'sans-serif'],
+            'title': ['Jockey One', 'sans-serif'],
+            'search': ['Julius Sans One', 'sans-serif'],
+         },
+         colors: {
+            'light': '#E3E3E3',
+            'gray-1': '#CFCFCF',
+            'gray-2': '#9A9A9A',
+            'dark': '#1C1C1C',
+         },
+         screens: {
+            'xs': '520px',
+         }
+      },
+   },
+   plugins: ["prettier-plugin-tailwindcss"],
 }
 

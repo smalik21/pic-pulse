15a516c9cf6e1bbd88928f3a64defe00fc56266d
smalik21 <sahilmalik25007@gmail.com>
2024-03-25 13:35:07 +0530

    fallback text

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -11,25 +11,36 @@ const MainSection = () => {
 
    useEffect(() => {
       console.log("Loaded...")
-   }, [images, videos])
+   }, [images, videos, type])
+
+   const fallback = <h1 className='absolute w-full mt-4 sm:mt-10 text-center text-lg sm:text-3xl text-gray-500'>No results to display!</h1>
 
    return (
       <main className='p-2 sm:p-4 mt-4 sm:mt-10 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
-         {type === "image" && images.map(image => {
-            return <img
-               key={image.imageId}
-               className='w-full h-full object-cover rounded-xl border border-slate-400'
-               src={image.previewURL}
-            />
-
-         })}
-         {type === "video" && videos.map(video => {
-            return <img
-               key={video.videoId}
-               className='w-full h-full object-cover rounded-xl border border-slate-400'
-               src={video.small.thumbnail}
-            />
-         })}
+         {type === "image" && (
+            images.length > 0 ? (
+               images.map(image => {
+                  return <img
+                     key={image.imageId}
+                     className='w-full h-full object-cover rounded-xl border border-slate-400'
+                     src={image.imageURL}
+                     loading='lazy'
+                  />
+               })
+            ) : fallback
+         )}
+         {type === "video" && (
+            videos.length > 0 ? (
+               videos.map(video => {
+                  return <img
+                     key={video.videoId}
+                     className='w-full h-full object-cover rounded-xl border border-slate-400'
+                     src={video.small.thumbnail}
+                     loading='lazy'
+                  />
+               })
+            ) : fallback
+         )}
       </main>
    )
 }

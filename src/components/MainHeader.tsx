2b4fd44ed846b4575dee3fa494d15ddbb829eb20
smalik21 <sahilmalik25007@gmail.com>
2024-03-25 09:03:47 +0530

    style

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -3,18 +3,18 @@ import PageOptions from "./PageOptions"
 import Search from "./Search"
 
 const MainHeader = () => {
-    return (
-        <header className="py-3 p-2 sm:p-4 text-white bg-heroSection bg-cover">
-            <section className="w-full flex justify-between">
-                <PageTitle />
-                <PageOptions />
-            </section>
-            <section className="w-full max-w-3xl mx-auto xs:w-4/5 sm:w-2/3 py-14 sm:py-16 lg:py-20">
-                <h3 id="search-header" className="w-full mb-2 sm:mb-4 lg:mb-6 sm:text-2xl text-center text-light font-search">SEARCH FREE IMAGES AND VIDEOS</h3>
-                <Search />
-            </section>
-        </header>
-    )
+   return (
+      <header className="py-3 p-2 sm:p-4 text-white bg-heroSection bg-cover sm:rounded-b-3xl">
+         <section className="w-full flex justify-between">
+            <PageTitle />
+            <PageOptions />
+         </section>
+         <section className="w-full max-w-3xl mx-auto xs:w-4/5 sm:w-2/3 py-14 sm:py-16 lg:py-20 sm:rounded-b-3xl">
+            <h3 id="search-header" className="w-full mb-2 sm:mb-4 lg:mb-6 sm:text-2xl text-center text-light font-search">SEARCH FREE IMAGES AND VIDEOS</h3>
+            <Search />
+         </section>
+      </header>
+   )
 }
 
 export default MainHeader
--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -14,11 +14,11 @@ const MainSection = () => {
    }, [images, videos])
 
    return (
-      <main className='p-2 sm:p-4 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
+      <main className='p-2 sm:p-4 mt-4 sm:mt-10 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
          {type === "image" && images.map(image => {
             return <img
                key={image.imageId}
-               className='w-full h-full object-cover'
+               className='w-full h-full object-cover rounded-xl border border-slate-400'
                src={image.previewURL}
             />
 
@@ -26,7 +26,7 @@ const MainSection = () => {
          {type === "video" && videos.map(video => {
             return <img
                key={video.videoId}
-               className='w-full h-full object-cover'
+               className='w-full h-full object-cover rounded-xl border border-slate-400'
                src={video.small.thumbnail}
             />
          })}

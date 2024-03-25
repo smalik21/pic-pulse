e14ec6546d41debf5b6c443c13fe39d73e3df9f7
smalik21 <sahilmalik25007@gmail.com>
2024-03-25 20:14:11 +0530

    add sticky header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,41 @@
-import { useEffect } from "react"
-import MainHeader from "../components/MainHeader"
-import MainSection from "../components/MainSection"
-import SearchType from "../components/SearchType"
+import { useEffect, useState } from "react"
 import { useImage } from "../hooks/useImage"
 import { useVideo } from "../hooks/useVideo"
 import { useParameter } from "../hooks/useParameter"
+import MainHeader from "../components/MainHeader"
+import SearchHeader from "../components/SearchHeader"
+import MainSection from "../components/MainSection"
+import SearchType from "../components/SearchType"
+import ScrollToTopButton from "../components/ScrollToTopButton"
 
 const HomePage = () => {
 
    const { loadImages } = useImage()
    const { loadVideos } = useVideo()
    const { reset, resetParameters } = useParameter()
+
+   const [showSearchHeader, setShowSearchHeader] = useState<boolean>(false)
+
+   useEffect(() => {
+      const handleScroll = () => {
+         // Check if MainHeader is scrolled out of view
+         const mainHeaderHeight = document.getElementById('mainHeader')!.offsetHeight
+
+         if (!mainHeaderHeight) return
+
+         if (window.scrollY >= mainHeaderHeight) {
+            setShowSearchHeader(true)
+         } else {
+            setShowSearchHeader(false)
+         }
+      }
+
+      window.addEventListener('scroll', handleScroll)
+
+      return () => {
+         window.removeEventListener('scroll', handleScroll)
+      }
+   }, [])
 
    const load = () => {
       loadImages('')
@@ -25,7 +50,9 @@ const HomePage = () => {
 
    return (
       <>
+         {showSearchHeader && <ScrollToTopButton />}
          <MainHeader />
+         {showSearchHeader && <SearchHeader />}
          <SearchType page="home" />
          <MainSection />
       </>

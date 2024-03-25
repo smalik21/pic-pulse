27c856eaa1021d863b1541b6259ed9a1c90c191c
smalik21 <sahilmalik25007@gmail.com>
2024-03-25 20:13:27 +0530

    add temp loader

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 const MainSection = () => {
 
    const { type } = useParameter()
-   const { images } = useImage()
+   const { images, loading } = useImage()
    const { videos } = useVideo()
 
    useEffect(() => {
@@ -17,29 +17,34 @@ const MainSection = () => {
 
    return (
       <main className='p-2 sm:p-4 mt-4 sm:mt-10 grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
-         {type === "image" && (
-            images.length > 0 ? (
-               images.map(image => {
-                  return <img
-                     key={image.imageId}
-                     className='w-full h-full object-cover rounded-xl border border-slate-400'
-                     src={image.imageURL}
-                     loading='lazy'
-                  />
-               })
-            ) : fallback
-         )}
-         {type === "video" && (
-            videos.length > 0 ? (
-               videos.map(video => {
-                  return <img
-                     key={video.videoId}
-                     className='w-full h-full object-cover rounded-xl border border-slate-400'
-                     src={video.small.thumbnail}
-                     loading='lazy'
-                  />
-               })
-            ) : fallback
+         {loading ? (
+            <h1>Loading...</h1>
+         ) : (
+            type === "image" ? (
+               images.length > 0 ? (
+                  images.map(image => (
+                     <img
+                        key={image.imageId}
+                        className='w-full h-full object-cover rounded-xl border border-slate-400'
+                        src={image.imageURL}
+                        loading='lazy'
+                     />
+                  ))
+               ) : fallback
+            ) : (
+               type === "video" && (
+                  videos.length > 0 ? (
+                     videos.map(video => (
+                        <img
+                           key={video.videoId}
+                           className='w-full h-full object-cover rounded-xl border border-slate-400'
+                           src={video.small.thumbnail}
+                           loading='lazy'
+                        />
+                     ))
+                  ) : fallback
+               )
+            )
          )}
       </main>
    )

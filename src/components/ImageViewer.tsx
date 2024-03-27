650d29930ebe237e6d82db3458cf1ce1332bbf78
smalik21 <sahilmalik25007@gmail.com>
2024-03-27 08:15:12 +0530

    feat: :sparkles: style image/video viewer

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -27,25 +27,36 @@ const ImageViewer = ({ image, setShowImageViewer }: ImageViewerPropTypes) => {
    }, [])
 
    return (
-      <div id="emptySpace" className="h-dvh w-full top-0 fixed flex justify-center items-center">
-         <article id="imageViewer" className="w-full xs:w-2/3 md:w-1/2 bg-red-400">
-            <span className="w-full flex justify-end">
-               <button onClick={handleClose} className="">Close</button>
+      <div id="emptySpace" className="h-dvh w-full top-0 fixed flex flex-col justify-start items-center bg-black bg-opacity-80 z-10">
+         <article id="imageViewer" className="w-full max-h-full mt-24 sm:w-2/3 rounded-xl overflow-y-scroll thin-scrollbar bg-light">
+            <span className="absolute -mt-8 sm:mt-0 sm:-ml-16">
+               <button onClick={handleClose} className="text-white border">Close</button>
             </span>
-            <section className="p-8 pt-0 gap-8 flex flex-col sm:flex-row justify-evenly">
-               <figure className="m-auto p-4">
+            <section className="p-2 mt-2 xs:p-4 xs:px-8 flex flex-col gap-4 sm:gap-8 items-center">
+               <section className="w-full flex flex-row justify-between">
+                  <button>Save</button>
+                  <button className="w-fit py-2 px-4 text-white bg-green-700 hover:bg-green-600 active:bg-green-800 rounded-md">Download</button>
+               </section>
+               <figure className="max-w-lg">
                   <img
                      src={image?.imageURL}
                      alt="image"
-                     className="w-full max-w-80 max-h-80"
+                     className="object-contain w-full h-full"
                   />
                </figure>
-               <section className="flex flex-col justify-between">
-                  <aside className="flex flex-wrap gap-4">
-                     {image?.imageTags.map(tag => <button key={tag}>{tag}</button>)}
-                  </aside>
-                  <button>Download</button>
-               </section>
+               <aside className="w-full mt-4 mb-8 flex flex-col xs:flex-row gap-4 items-center">
+                  <h1 className="xs:text-xl font-bold">More Like This: </h1>
+                  <section className="flex flex-wrap gap-4">
+                     {image?.imageTags.map(tag => {
+                        return <button
+                           key={tag}
+                           className="px-3 py-1.5 xs:px-4 xs:py-2 text-nowrap text-xs sm:text-base border border-black rounded-md"
+                        >
+                           {tag}
+                        </button>
+                     })}
+                  </section>
+               </aside>
             </section>
          </article>
       </div>
--- src/components/VideoViewer.tsx
+++ src/components/VideoViewer.tsx
@@ -27,24 +27,37 @@ const VideoViewer = ({ video, setShowVideoViewer }: VideoViewerPropTypes) => {
    }, [])
 
    return (
-      <div id="emptySpace" className="h-dvh w-full top-0 fixed flex justify-center items-center">
-         <article id="videoViewer" className="w-full xs:w-2/3 md:w-1/2 bg-red-400">
-            <span className="w-full flex justify-end">
-               <button onClick={handleClose} className="">Close</button>
+      <div id="emptySpace" className="h-dvh w-full top-0 fixed flex flex-col justify-start items-center bg-black bg-opacity-80 z-10">
+         <article id="videoViewer" className="w-full max-h-full mt-24 sm:w-2/3 rounded-xl overflow-y-scroll thin-scrollbar bg-light">
+            <span className="absolute -mt-8 sm:mt-0 sm:-ml-16">
+               <button onClick={handleClose} className="text-white border">Close</button>
             </span>
-            <section className="p-8 pt-0 gap-8 flex flex-col sm:flex-row justify-evenly">
-               <figure className="m-auto p-4">
-                  <video controls>
-                     <source src={video?.normal.videoURL} />
-                     Your browser does not support the video tag.
-                  </video>
-               </figure>
-               <section className="flex flex-col justify-between">
-                  <aside className="flex flex-wrap gap-4">
-                     {video?.videoTags.map(tag => <button key={tag}>{tag}</button>)}
-                  </aside>
-                  <button>Download</button>
+            <section className="p-2 mt-2 xs:p-4 xs:px-8 flex flex-col gap-4 sm:gap-8 items-center">
+               <section className="w-full flex flex-row justify-between">
+                  <button>Save</button>
+                  <button className="w-fit py-2 px-4 text-white bg-green-700 hover:bg-green-600 active:bg-green-800 rounded-md">Download</button>
                </section>
+               <figure className="max-w-lg">
+                  <div className="aspect-video">
+                     <video className="w-full h-full object-contain" controls>
+                        <source src={video?.normal.videoURL} />
+                        Your browser does not support the video tag.
+                     </video>
+                  </div>
+               </figure>
+               <aside className="w-full mt-4 mb-8 flex flex-col xs:flex-row gap-4 items-center">
+                  <h1 className="xs:text-xl font-bold">More Like This: </h1>
+                  <section className="flex flex-wrap gap-4">
+                     {video?.videoTags.map(tag => {
+                        return <button
+                           key={tag}
+                           className="px-3 py-1.5 xs:px-4 xs:py-2 text-nowrap text-xs sm:text-base border border-black rounded-md"
+                        >
+                           {tag}
+                        </button>
+                     })}
+                  </section>
+               </aside>
             </section>
          </article>
       </div>

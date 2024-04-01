b9cdbc1a4dc630c4a7fddd7c95e0cf31c9ff829d
smalik21 <sahilmalik25007@gmail.com>
2024-04-01 06:51:24 +0530

    feat: user profile page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage"
 import SearchPage from "./pages/SearchPage"
 import SignupPage from "./pages/SignupPage"
 import LoginPage from "./pages/LoginPage"
+import UserPage from "./pages/UserPage"
 
 export default function App() {
 
@@ -14,6 +15,7 @@ export default function App() {
                <Route path="/search/:q" element={<SearchPage />} />
                <Route path="/signup" element={<SignupPage />} />
                <Route path="/login" element={<LoginPage />} />
+               <Route path="/user/*" element={<UserPage />} />
                <Route path="*" element={<Navigate to={'/'} />} />
             </Routes>
          </Router>
---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,89 @@
+import { Route, Routes, useNavigate, NavLink, Navigate } from "react-router-dom"
+import PageTitle from "../components/PageTitle"
+import { useAuth } from "../hooks/useAuth"
+import ProfileDefaultImg from "../assets/profile-image.svg"
+
+const UserPage = () => {
+   const { isAuthenticated, logout } = useAuth()
+   const navigate = useNavigate()
+
+   const handleLogoutClick = () => {
+      if (isAuthenticated) {
+         logout()
+            .then(() => navigate('/login'))
+      }
+   }
+
+   return (
+      <>
+         <header id="authHeader" className="py-3 p-2 sm:p-4 text-white bg-heroSection bg-cover">
+            <section className="w-full flex justify-between">
+               <PageTitle />
+               <button onClick={handleLogoutClick} className="px-4 py-1 sm:py-2 text-sm flex items-center rounded-md text-white bg-red-700 hover:bg-red-500 active:bg-red-900">
+                  Logout
+               </button>
+            </section>
+            <section id="profile" className="w-full py-4 sm:py-8 flex justify-center">
+               <figure className="flex flex-col gap-2 sm:gap-4 sm:text-lg items-center">
+                  <img
+                     id="profile-img"
+                     src={ProfileDefaultImg}
+                     className=""
+                  />
+                  <p>Profile Name</p>
+               </figure>
+            </section>
+         </header>
+         <main className="p-2 sm:p-4">
+            <nav className="sm:px-8">
+               <ul className="flex justify-center sm:justify-start gap-4 sm:gap-6">
+                  <li>
+                     <NavLink
+                        to={`/user/profile`}
+                        className={({ isActive }) =>
+                           `block px-4 py-2 text-sm sm:text-md sm:px-6 sm:py-3
+                           ${isActive ? 'border border-b-0 rounded-t-lg border-black' : ''
+                           }`
+                        }
+                     >
+                        Profile
+                     </NavLink>
+                  </li>
+                  <li>
+                     <NavLink
+                        to={`/user/account`}
+                        className={({ isActive }) =>
+                           `block px-4 py-2 text-sm sm:text-md sm:px-6 sm:py-3
+                           ${isActive ? 'border border-b-0 rounded-t-lg border-black' : ''
+                           }`
+                        }
+                     >
+                        Account
+                     </NavLink>
+                  </li>
+                  <li>
+                     <NavLink
+                        to={`/user/saved`}
+                        className={({ isActive }) =>
+                           `block px-4 py-2 text-sm sm:text-md sm:px-6 sm:py-3
+                           ${isActive ? 'border border-b-0 rounded-t-lg border-black' : ''
+                           }`
+                        }
+                     >
+                        Saved
+                     </NavLink>
+                  </li>
+               </ul>
+            </nav>
+            <Routes>
+               <Route path="/profile" element={<p className="text-xl text-red-300">profile</p>} />
+               <Route path="/account" element={<p className="text-xl text-red-300">account</p>} />
+               <Route path="/saved" element={<p className="text-xl text-red-300">saved</p>} />
+               <Route path="*" element={<Navigate to={'/user/profile'} />} />
+            </Routes>
+         </main>
+      </>
+   )
+}
+
+export default UserPage

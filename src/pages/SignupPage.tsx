95778f71cbb5afd6c6816e9a48cee6acaab414aa
smalik21 <sahilmalik25007@gmail.com>
2024-04-07 20:18:49 +0530

    refactor

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -6,8 +6,10 @@ import AuthHeader from "../components/headers/AuthHeader"
 const SignupPage = () => {
 
    const [error, setError] = useState<string>('')
-   const usernameRef = useRef<HTMLInputElement>(null)
-   const passwordRef = useRef<HTMLInputElement>(null)
+   // const nameRef = useRef<HTMLInputElement>(null)
+   const emailRef = useRef<HTMLInputElement>(null)
+   const passRef = useRef<HTMLInputElement>(null)
+   const confirmPassRef = useRef<HTMLInputElement>(null)
 
    const navigate = useNavigate()
    const { signup, isAuthenticated } = useAuth()
@@ -18,13 +20,29 @@ const SignupPage = () => {
 
    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-      const username = usernameRef.current?.value
-      const password = passwordRef.current?.value
-      if (!username || !password) return
-      console.log("username:", username)
+      // const name = nameRef.current?.value.trim()
+      const email = emailRef.current?.value.trim()
+      const password = passRef.current?.value
+      const confirmPassword = confirmPassRef.current?.value
+
+      if (!email || !password) return
+
+      // Password validation
+      if (password && password.length < 8) {
+         alert('New password should be at least 8 characters long.')
+         passRef.current?.focus()
+         return
+      }
+      if (password && confirmPassword !== password) {
+         alert('Passwords do not match.')
+         passRef.current?.focus()
+         return
+      }
+
+      console.log("username:", email)
       console.log("password:", password)
 
-      signup(username, password)
+      signup(email, password)
          .then(() => navigate('/login'))
          .catch((error) => setError(error))
    }
@@ -33,25 +51,75 @@ const SignupPage = () => {
       <>
          <AuthHeader page="signup" />
          <main className="w-full h-dvh flex justify-center items-center bg-blue-400">
-            <article id="signup-card" className="w-full xs:w-3/5 max-w-96 flex flex-col bg-white">
+            <form
+               onSubmit={handleSubmit}
+               className="w-full max-w-sm flex flex-col sm:gap-2 border-black"
+            >
                <h1 className="w-full text-center border">Sign Up</h1>
                {error && <p>{error.toString()}</p>}
-               <form
-                  onSubmit={handleSubmit}
-                  className="flex flex-col"
+               {/* <div className="mb-4">
+                  <label htmlFor="fullName" className="block font-bold mb-2">
+                     Full Name
+                  </label>
+                  <input
+                     type="text"
+                     id="fullName"
+                     className="w-full px-4 py-2 border border-slate-400 rounded-lg"
+                     placeholder="Enter your name"
+                     ref={nameRef}
+                     required
+                  />
+               </div> */}
+               <div className="mb-4">
+                  <label htmlFor="email" className="block font-bold mb-2">
+                     Email
+                  </label>
+                  <input
+                     type="email"
+                     id="email"
+                     className="w-full px-4 py-2 border border-slate-400 rounded-lg"
+                     placeholder="Enter your email"
+                     ref={emailRef}
+                     required
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label htmlFor="new-password" className="block font-bold mb-2">
+                     New Password
+                  </label>
+                  <input
+                     type="password"
+                     id="new-password"
+                     className="w-full px-4 py-2 border border-slate-400 rounded-lg"
+                     placeholder="Enter your password"
+                     ref={passRef}
+                     required
+                  />
+               </div>
+
+               <div className="mb-4">
+                  <label htmlFor="confirm-password" className="block font-bold mb-2">
+                     Confirm Password
+                  </label>
+                  <input
+                     type="password"
+                     id="confirm-password"
+                     className="w-full px-4 py-2 border border-slate-400 rounded-lg"
+                     placeholder="Confirm your password"
+                     ref={confirmPassRef}
+                     required
+                  />
+               </div>
+
+               <button
+                  type="submit"
+                  className="bg-green-600 hover:bg-green-400 active:bg-green-800 border"
                >
-                  <label htmlFor="username">Username:</label>
-                  <input id="username" ref={usernameRef} type="text" required />
-                  <label htmlFor="password">Password:</label>
-                  <input id="password" ref={passwordRef} type="password" required />
-                  <button
-                     type="submit"
-                     className="bg-green-600 hover:bg-green-400 active:bg-green-800 border"
-                  >
-                     Submit
-                  </button>
-               </form>
-            </article>
+                  Submit
+               </button>
+
+            </form>
          </main>
       </>
    )

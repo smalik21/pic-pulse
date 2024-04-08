6d0c154157b2890f40824686a722ff39a2b898dc
smalik21 <sahilmalik25007@gmail.com>
2024-04-08 16:39:59 +0530

    remove login/signup button from header

--- src/components/headers/AuthHeader.tsx
+++ src/components/headers/AuthHeader.tsx
@@ -1,17 +1,10 @@
 import PageTitle from "../PageTitle"
-import { Link } from "react-router-dom"
 
-const AuthHeader = ({ page }: { page: string }) => {
+const AuthHeader = () => {
    return (
-      <header id="authHeader" className="py-3 p-2 sm:p-4 text-white bg-heroSection bg-cover">
+      <header id="authHeader" className="absolute w-full py-3 p-2 sm:p-4 text-white bg-cover">
          <section className="w-full flex justify-between">
             <PageTitle />
-            <Link
-               to={page === "signup" ? "/login" : "/signup"}
-               className="px-4 py-1 sm:py-2 text-sm flex items-center rounded-md text-black bg-white hover:bg-slate-200"
-            >
-               {page === "signup" ? "Login" : "Signup"}
-            </Link>
          </section>
       </header>
    )

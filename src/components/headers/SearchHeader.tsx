d9a3c8cda664ca6e8c5baf7486f94e0155f1adc3
smalik21 <sahilmalik25007@gmail.com>
2024-04-10 14:11:39 +0530

    fix: :bug: search header top margin

--- src/components/headers/SearchHeader.tsx
+++ src/components/headers/SearchHeader.tsx
@@ -4,9 +4,9 @@ import Search from "../Search"
 
 const SearchHeader = () => {
    return (
-      <header className="py-3 p-2 sm:p-4 gap-4 lg:gap-8 flex flex-wrap justify-between items-center text-white bg-heroSection bg-cover shadow-lg shadow-gray-800 sticky top-0 z-10">
+      <header className="py-3 p-2 sm:p-4 gap-4 lg:gap-8 flex flex-wrap sm:flex-nowrap justify-between items-center text-white bg-heroSection bg-cover shadow-lg shadow-gray-800 sticky -top-1 z-10">
          <PageTitle />
-         <span className="lg:order-last"><PageOptions /></span>
+         <span className="sm:order-last"><PageOptions /></span>
          <section className="w-full mx-auto xs:w-4/5 sm:w-2/3 lg:w-fit lg:grow">
             <Search />
          </section>
